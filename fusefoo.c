a0da4a7efe88b2a3ddc08e15a66fca5318db13cc
Fabian Bornhofen <fbornhofen@gmail.com>
2013-05-25 04:13:31 +0200

    custom mainloop

--- fusefoo.c
+++ fusefoo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <time.h>
 
 #define FUSE_USE_VERSION 26
 #include <fuse.h>
@@ -60,6 +61,26 @@ int hello_readdir(const char *path, void *buf, fuse_fill_dir_t fill, off_t offse
     return 0;
 }
 
+void hello_mainloop(int argc, char** argv, struct fuse_operations* op, void* user_data)
+{
+    struct fuse *f;
+    char* mountpoint;
+    int mt;
+    struct timespec interval;
+    interval.tv_sec = 0;
+    interval.tv_nsec = 20000000;
+    f = fuse_setup(argc, argv, op, sizeof(struct fuse_operations), &mountpoint, &mt, user_data);
+    for (;;) {
+        struct fuse_cmd* cmd = fuse_read_cmd(f);
+        if (fuse_exited(f)) {
+            break;
+        }
+        fuse_process_cmd(f, cmd);
+        nanosleep(&interval, NULL);
+    }
+    fuse_teardown(f, mountpoint);
+}
+
 struct fuse_operations hellofuse_ops = {
     .getattr = hello_getattr,
     .opendir = hello_opendir,
@@ -71,6 +92,7 @@ struct fuse_operations hellofuse_ops = {
 int main(int argc, char** argv)
 {
     printf("Hello FUSE\n");
-    fuse_main(argc, argv, &hellofuse_ops, NULL);
+    //fuse_main(argc, argv, &hellofuse_ops, NULL);
+    hello_mainloop(argc, argv, &hellofuse_ops, NULL);
     return 0;
 }
